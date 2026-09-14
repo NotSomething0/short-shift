2f86d4bb97b66e410714f3edfd2b59cd97734f2c
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-09-14 12:36:43 -0500

    fix(database/series): updateSeries should take a Series object

--- src/database/series/updateSeries.ts
+++ src/database/series/updateSeries.ts
@@ -1,23 +1,15 @@
-import { ActionError, type ActionAPIContext } from 'astro:actions';
-import { handleAuthError, handlePostgrestError } from '$lib/supabase';
+import type { Series } from ".";
+import { type ActionAPIContext } from "astro:actions";
+import { handlePostgrestError } from "$lib/supabase";
 
-export default async(context: ActionAPIContext, series: { id: string, name: string, description: string }) => {
-    const { data: claimsData, error: claimsError } = await context.locals.supabase.auth.getClaims();
+export default async (context: ActionAPIContext, series: Series) => {
+  const { data, error } = await context.locals.supabase
+    .from("series")
+    .upsert(series)
+    .select()
+    .single();
 
-    if (claimsError)
-        handleAuthError(claimsError);
+  if (error) handlePostgrestError(error);
 
-    if (!claimsData?.claims?.app_metadata?.admin)
-        throw new ActionError({ code: 'UNAUTHORIZED', message: 'You are not authorized to perform this action'});
-
-    const { data, error } = await context.locals.supabase
-        .from('series')
-        .upsert(series)
-        .select()
-        .single();
-
-    if (error)
-        handlePostgrestError(error);
-
-    return data;
-}
+  return data;
+};
