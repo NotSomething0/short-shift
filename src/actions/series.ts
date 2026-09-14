f38ba52ff4b1a9cfb5f010177d822f6ff890432e
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-09-14 12:36:23 -0500

    fix(actions/series): make updateSeries take a custom zod object of Series

--- src/actions/series.ts
+++ src/actions/series.ts
@@ -1,6 +1,7 @@
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro/zod";
 import * as db from "../database/series";
+import { handleAuthError } from "$lib/supabase";
 
 export default {
   createSeries: defineAction({
@@ -24,12 +25,18 @@ export default {
     },
   }),
   updateSeries: defineAction({
-    input: z.object({
-      id: z.string(),
-      name: z.string(),
-      description: z.string(),
-    }),
+    input: z.custom<db.Series>(),
     handler: async (input, context) => {
+      const { data, error } = await context.locals.supabase.auth.getClaims();
+
+      if (error) handleAuthError(error);
+
+      if (!data?.claims.app_metadata?.admin)
+        throw new ActionError({
+          code: "UNAUTHORIZED",
+          message: "You are not authorized to perform this action.",
+        });
+
       return await db.updateSeries(context, input);
     },
   }),
